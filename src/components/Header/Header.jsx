d9a999bc01221a8e6878b438b7449f22efa8f30b
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-04-05 00:32:46 +0600

    hamburger menu in navbar has added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ActiveLink from "../ActiveLink/ActiveLink";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <nav className=" bg-cyan-700 p-4 text-white">
-      <ul className="md:flex gap-4">
+    <nav className=" bg-cyan-700 pb-20 pl-4 text-white">
+      <button onClick={() => setOpen(!open)} className="md:hidden">
+        <span>
+          {open === true ? (
+            <XMarkIcon className="h-6 w-6 text-cyan-400" />
+          ) : (
+            <Bars3Icon className="h-6 w-6 text-cyan-400" />
+          )}
+        </span>
+      </button>
+
+      <ul
+        className={`md:flex md:pt-10 gap-4 absolute md:static duration-500 ${
+          open ? "top-6" : "-top-48"
+        }`}
+      >
         <li>
           {" "}
           <ActiveLink to="/">Home</ActiveLink>
