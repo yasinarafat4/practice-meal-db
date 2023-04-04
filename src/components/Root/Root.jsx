299e095579fe6d9c9779df7893b00ff9e271fe1d
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-04-05 00:49:29 +0600

    laoding added

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 
 const Root = () => {
+  const navigation = useNavigation();
   return (
     <div>
       <Header></Header>
-
+      <div className="text-center text-4xl text-red-600 font-bold">
+        {navigation.state === "loading" ? "Loading..." : ""}
+      </div>
       <Outlet></Outlet>
     </div>
   );
